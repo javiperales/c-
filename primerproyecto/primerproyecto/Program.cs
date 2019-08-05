0b2c0528538d8768db9df701f2f1329bab497761
javi perales <javiperales95@gmail.com>
2019-08-05 13:35:50 +0200

    hola mundo

---
+++ primerproyecto/primerproyecto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace primerproyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bienvenidos");
+            
+        }
+    }
+}
